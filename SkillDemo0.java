d43768a2219a6d22461965fb27bc3560da568c4d
tylermeyers <88351548+tylermeyers@users.noreply.github.com>
2022-02-04 15:55:13 -0800

    fial test

--- SkillDemo0.java
+++ SkillDemo0.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo0{
+    public static int minus(int n1, int n2 ){
+        return (n1-n2);
+    }
+}
---
+++ SkillDemo0Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo0Test {
+    @Test
+    public void minus(){
+        assertEquals(2,SkillDemo0.minus(4,2));
+    }
+}
